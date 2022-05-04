58e0fb77d79f9c6858eae28310306f7558be4575
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-04 18:09:58 -0500

    Test

--- Assets/Enemies/Move.cs
+++ Assets/Enemies/Move.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+ 
+public class Movement : MonoBehaviour
 {
+ 
+    public float distance;
+    public float spinSpeed;
+    public float linearSpeed;
+    private Rigidbody2D rgb2d;
+    private float startPosition;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rgb2d = GetComponent<Rigidbody2D>();
+        startPosition = rgb2d.position.x;
     }
-
-    // Update is called once per frame
-    void Update()
+ 
+    private void FixedUpdate()
     {
-        
+        rgb2d.SetRotation(rgb2d.rotation + spinSpeed * Time.fixedDeltaTime);
+        rgb2d.MovePosition(new Vector2((Mathf.Sin((2 * Mathf.PI * (Time.time*linearSpeed/distance)) - (Mathf.PI / 2)) * (distance/2) + (distance/2))+startPosition,rgb2d.position.y));
     }
 }
