97216c7d81339b3527e3b7900c39899264b767a0
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-04 21:04:42 -0500

    test

--- Assets/Enemies/Move.cs
+++ Assets/Enemies/Move.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SpriteRenderer.Sprite
+using UnityEngine.SpriteRenderer.Sprite;
  
 public class Move : MonoBehaviour
 {
