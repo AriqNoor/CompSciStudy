443c789b06cca8618343e701a574f0718eb751f5
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-05 15:39:03 -0500

    Square

--- Assets/Enemies/Move.cs
+++ Assets/Enemies/Move.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     // I plan to gradually change speed
-    public float speed = 10f;
+    public float speed = 20f;
 
     bool change = true;
 
