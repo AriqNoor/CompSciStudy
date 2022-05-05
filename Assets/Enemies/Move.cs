d620fefdee78884ab47cda3381e3aacf6a71d667
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-04 20:37:25 -0500

    test

--- Assets/Enemies/Move.cs
+++ Assets/Enemies/Move.cs
@@ -6,20 +6,41 @@ public class Move : MonoBehaviour
 {
     public float speed = 5f;
 
+    bool change = true;
+    private SpriteRenderer SpriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update() 
     {
-        movesquareright();
+        if(change) {
+            movesquareright();
+        }
+        if(!change) {
+            movesquareleft();
+        }
+        if(transform.position.x >= 9f) {
+            change = false;
+            spriteRenderer.flipC = true;
+        }
+        if(transform.position.c <= -9f) {
+            change = true;
+            spriteRenderer.flipX = false;
+        }
+        
     }
 
     void movesquareright() {
         transform.Translate(speed*Time.deltaTime,0,0);
+    }
+
+    void movesquareleft() {
+        transform.Translate(-speed*Time.deltaTime,0,0);
     }
 
 }
