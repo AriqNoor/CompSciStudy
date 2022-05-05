91da56f9781897a914b5d61cb760f230ddde1d84
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-04 20:29:50 -0500

    test

--- Assets/Enemies/Move.cs
+++ Assets/Enemies/Move.cs
@@ -6,14 +6,16 @@ public class Move : MonoBehaviour
 {
     public float speed = 5f;
 
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update() 
     {
-        movesquareright
+        movesquareright();
     }
 
     void movesquareright() {
