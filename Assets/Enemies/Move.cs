60b4347e1ab47584df554f683715add145a06968
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-05 15:33:02 -0500

    Test

--- Assets/Enemies/Move.cs
+++ Assets/Enemies/Move.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Sprite;
  
 public class Move : MonoBehaviour
 {
@@ -9,7 +8,7 @@ public class Move : MonoBehaviour
     public float speed = 5f;
 
     bool change = true;
-    private SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
