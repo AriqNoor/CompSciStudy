c4d2590faa95b2007a8dbb51d2aad77ce77298bd
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-05 15:34:10 -0500

    test

--- Assets/Enemies/Move.cs
+++ Assets/Enemies/Move.cs
@@ -8,7 +8,7 @@ public class Move : MonoBehaviour
     public float speed = 5f;
 
     bool change = true;
-    public SpriteRenderer sr;
+    public SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
