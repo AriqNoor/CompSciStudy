dcfc7a590592cf3ebfaf409ef8c066a528a8b893
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-09 18:30:39 -0500

    Comments

--- Assets/Win.cs
+++ Assets/Win.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Win : MonoBehaviour
 {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // This destroys the Circle GameObject and writes stated message under the console if the circle reaches a certain point on the Y - axis
         if(transform.position.y >= 5f) {
             Debug.Log("CONGRATULATIONS! YOU WIN! START OVER IF YOU WANT!");
             Destroy(GameObject.Find("Circle"));
