6d2ba71e3e7ef847e1b917215239ebef3c43e7c2
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-09 18:14:59 -0500

    Comments Circle

--- Assets/Enemies/Player.cs
+++ Assets/Enemies/Player.cs
@@ -6,22 +6,32 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    // Create Rigidbody2D variable called rb
     private Rigidbody2D rb;
+
+    // Create Vector2 variable called moveVelocity
     private Vector2 moveVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Sets rb variable = Rigidbody2D that's attached to the circle GameObject
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // The moveInput allows the circle to be moved based on the "Horizontal" and "Vertical" inputs
+        // "Horizontal" are the left and right arrow keys
+        // "Vertical" are the up and down arrow keys
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        // MoveInput.normalized allows for diagonal movement
         moveVelocity = moveInput.normalized * speed;
     }
 
+    // Moves the circle
     void FixedUpdate() {
         rb.MovePosition(rb.position+moveVelocity*Time.fixedDeltaTime);
     }
