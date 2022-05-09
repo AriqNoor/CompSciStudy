2b9c702bc0e3e528c74b5442aef4f9ddd854307d
AriqNoor <ariq.noor@student.allenisd.org>
2022-05-09 18:29:12 -0500

    Comments

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Circle : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // This destroys the Circle GameObject and writes stated message under the console
     private void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("GAME OVER! TRY AGAIN");
         Destroy(GameObject.Find("Circle"));
