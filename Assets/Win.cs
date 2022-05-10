e0e61e4f31bba9d4205c42074ceacf017b5e47c1
Guo Xu <guo.xu@student.allenisd.org>
2022-05-10 00:11:19 -0500

    D

--- Assets/Win.cs
+++ Assets/Win.cs
@@ -16,7 +16,7 @@ public class Win : MonoBehaviour
     {
         // This destroys the Circle GameObject and writes stated message under the console if the circle reaches a certain point on the Y - axis
         if(transform.position.y >= 5f) {
-            Debug.Log("CONGRATULATIONS! YOU WIN! START OVER IF YOU WANT!");
+            Debug.Log("CONGRATULATIONS! YOU WIN! START OVER IF YOU WANT!");//congratulations when you pass all the squares
             Destroy(GameObject.Find("Circle"));
         }
     }
