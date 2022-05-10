abb160e5b000c25e80000ff63a144c7158019b03
Guo Xu <guo.xu@student.allenisd.org>
2022-05-10 00:09:25 -0500

    documentation

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -18,7 +18,7 @@ public class Circle : MonoBehaviour
 
     // This destroys the Circle GameObject and writes stated message under the console if the circle collides with any of the squares
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("GAME OVER! TRY AGAIN");
-        Destroy(GameObject.Find("Circle"));
+        Debug.Log("GAME OVER! TRY AGAIN");//whenever the ball collides with the squares, the ball gets destroyed and it says "game over"
+        Destroy(GameObject.Find("Circle"));// whenever the ball gets past all the squares, it says congrats you won.
     }
 }
